85b86685a99254748ce3603f0a5afc94e851b96b
clmntgbr <clement.goubier@gmail.com>
2025-02-28 13:49:03 +0100

    Add Makefile for code formatting and linting; update requirements and refactor code for consistency

--- src/main.py
+++ src/main.py
@@ -20,24 +20,26 @@ s3_client = S3Client(Config)
 rmq_client = RabbitMQClient()
 file_client = FileClient()
 
-celery = Celery(
-    'tasks',
-    broker=app.config['RABBITMQ_URL']
+celery = Celery("tasks", broker=app.config["RABBITMQ_URL"])
+
+celery.conf.update(
+    {
+        "task_serializer": "json",
+        "accept_content": ["json"],
+        "broker_connection_retry_on_startup": True,
+        "task_routes": {
+            "tasks.process_message": {"queue": app.config["RMQ_QUEUE_WRITE"]}
+        },
+        "task_queues": [
+            Queue(
+                app.config["RMQ_QUEUE_READ"], routing_key=app.config["RMQ_QUEUE_READ"]
+            )
+        ],
+    }
 )
 
-celery.conf.update({
-    'task_serializer': 'json',
-    'accept_content': ['json'],
-    'broker_connection_retry_on_startup': True,
-    'task_routes': {
-        'tasks.process_message': {'queue': app.config['RMQ_QUEUE_WRITE']}
-    },
-    'task_queues': [
-        Queue(app.config['RMQ_QUEUE_READ'], routing_key=app.config['RMQ_QUEUE_READ'])
-    ],
-})
 
-@celery.task(name='tasks.process_message', queue=app.config['RMQ_QUEUE_READ'])
+@celery.task(name="tasks.process_message", queue=app.config["RMQ_QUEUE_READ"])
 def process_message(message):
     mediaPod: MediaPod = ProtobufConverter.json_to_protobuf(message)
     protobuf = ApiToSoundExtractor()
@@ -54,9 +56,9 @@ def process_message(message):
 
         audioFilePath = uuid + ".mp3"
         tmpAudioFilePath = f"/tmp/{audioFilePath}"
-        
+
         probe = ffmpeg.probe(tmpFilePath)
-        duration = float(probe['format']['duration'])
+        duration = float(probe["format"]["duration"])
 
         if not extract_sound(tmpFilePath, tmpAudioFilePath):
             return False
@@ -66,7 +68,9 @@ def process_message(message):
         chunks = chunk_wav(audioFilePath, uuid)
 
         for chunk in chunks:
-            key = f"{protobuf.mediaPod.userUuid}/{protobuf.mediaPod.uuid}/audios/{chunk}"
+            key = (
+                f"{protobuf.mediaPod.userUuid}/{protobuf.mediaPod.uuid}/audios/{chunk}"
+            )
             if not s3_client.upload_file(f"/tmp/{chunk}", key):
                 return False
             file_client.delete_file(f"/tmp/{chunk}")
@@ -78,15 +82,20 @@ def process_message(message):
 
         protobuf.mediaPod.originalVideo.length = int(duration)
         protobuf.mediaPod.originalVideo.audios.extend(resultsSorted)
-        protobuf.mediaPod.status = MediaPodStatus.Name(MediaPodStatus.SOUND_EXTRACTOR_COMPLETE)
-        
+        protobuf.mediaPod.status = MediaPodStatus.Name(
+            MediaPodStatus.SOUND_EXTRACTOR_COMPLETE
+        )
+
         rmq_client.send_message(protobuf, "App\\Protobuf\\SoundExtractorToApi")
 
         return True
-    except Exception as e:
-        protobuf.mediaPod.status = MediaPodStatus.Name(MediaPodStatus.SOUND_EXTRACTOR_ERROR)
+    except Exception:
+        protobuf.mediaPod.status = MediaPodStatus.Name(
+            MediaPodStatus.SOUND_EXTRACTOR_ERROR
+        )
         if not rmq_client.send_message(protobuf, "App\\Protobuf\\SoundExtractorToApi"):
             return False
+
 
 def extract_sound(file: str, audioFilePath: str) -> bool:
     try:
@@ -97,22 +106,27 @@ def extract_sound(file: str, audioFilePath: str) -> bool:
         print(f"error extracting audio: {e}")
     return False
 
+
 def extract_chunk_number(item):
-    match = re.search(r'_(\d+)\.wav$', item[0])
-    return int(match.group(1)) if match else float('inf')
+    match = re.search(r"_(\d+)\.wav$", item[0])
+    return int(match.group(1)) if match else float("inf")
+
 
 def convert_to_wav(audioFilePath) -> str:
     audio = AudioSegment.from_mp3(audioFilePath)
     wav_path = audioFilePath.replace(".mp3", ".wav")
-    audio.export(wav_path, format="wav", parameters=["-ac", "1", "-ar", "16000"]) 
+    audio.export(wav_path, format="wav", parameters=["-ac", "1", "-ar", "16000"])
     return wav_path
 
-def chunk_wav(audioFilePath: str, uuid: str) -> list[str]: 
+
+def chunk_wav(audioFilePath: str, uuid: str) -> list[str]:
     audio = AudioSegment.from_mp3(audioFilePath)
     segmentDuration = 5 * 60 * 1000
     chunkFilenames = []
 
-    chunks = [audio[i:i+segmentDuration] for i in range(0, len(audio), segmentDuration)]
+    chunks = [
+        audio[i : i + segmentDuration] for i in range(0, len(audio), segmentDuration)
+    ]
     for idx, chunk in enumerate(chunks):
         chunk.export(f"/tmp/{uuid}_{idx+1}.wav", format="wav")
         chunkFilenames.append(f"{uuid}_{idx+1}.wav")
